6e1f629f70b6db33e190911fcd3d42620d198b9b
Valentin Schneider <valentin.schneider@arm.com>
2018-04-15 17:56:34 +0100

    whitespace-cleanup

--- tla-mode.el
+++ tla-mode.el
@@ -17,11 +17,11 @@
 (defvar tla-mode-constants
     '("FALSE"
       "TRUE"))
-  
+
 (defvar tla-mode-keywords
   '("MODULE" "EXTENDS" "INSTANCE" "WITH"
     "LOCAL"
-    "CONSTANT" "CONSTANTS" "VARIABLE" "VARIABLES" 
+    "CONSTANT" "CONSTANTS" "VARIABLE" "VARIABLES"
     "IF" "THEN" "ELSE"
     "CHOOSE" "CASE" "OTHER"
     "LET"
@@ -49,7 +49,7 @@
     (modify-syntax-entry ?\( ". 1" st)
     (modify-syntax-entry ?* ". 23" st)
     (modify-syntax-entry ?\) ". 4" st)
-    
+
     ; Return st
     st
     )
@@ -72,92 +72,92 @@
 (define-derived-mode tla-mode prog-mode "TLA"
   "TLA mode is a major mode for writing TLA+ specifications"
   :syntax-table tla-mode-syntax-table
-  
+
   (setq-local font-lock-defaults tla-font-lock-defaults)
-  
+
   ;; for writing comments (not fontifying them)
   (setq-local comment-start "(*")
   (setq-local comment-end "*)")
 
   (setq-local prettify-symbols-alist
-              '(
-                ("/\\" . ?∧)          ("\\land" . ?∧)
-                ("\\/" . ?∨)          ("\\lor" . ?∨)
-                ("=>" . ?⇒)
-                ("~" . ?¬)
-                ("\\lnot" . ?¬)       ("\\neg" . ?¬) 
-                ("<=>" . ?≡)          ("\equiv" . ?≡)
-                ("==" . ?≜)
-                ("\\in" . ?∈)
-                ("\\notin" . ?∉)
-                ("#" . ?≠)            ("/=" . ?≠)
-                ("<<" . ?⟨)
-                (">>" . ?⟩)
-                ("[]" . ?□)
-                ("<>" . ?◇)
-                ("<=" . ?≤)           ("\\leq" . ?≤)
-                (">=" . ?≥)           ("\\geq" . ?≥)
-                ("~>" . ?↝)
-                ("\\ll" . ?\《)
-                ("\\gg" . ?\》)
-                ("-+->" . ?⇸)
-                ("\\prec" . ?≺)
-                ("\\succ" . ?≻)
-                ("|->" . ?↦)
-                ("\\preceq" . ?⋞)
-                ("\\succeq" . ?≽)
-                ("\\div" . ?÷)
-                ("\\subseteq" . ?⊆)
-                ("\\supseteq" . ?⊇)
-                ("\\cdot" . ?⋅)
-                ("\\subset" . ?⊂)
-                ("\\supset" . ?⊃)
-                ("\\o" . ?∘) ("\\circ" . ?∘) 
-                ("\\sqsubset" . ?⊏)
-                ("\\sqsupset" . ?⊐)
-                ("\\bullet" . ?•)
-                ("\\sqsubseteq" . ?⊑)
-                ("\\sqsupseteq" . ?⊒)
-                ("\\star" . ?⋆)
-                ("|-" . ?⊢)
-                ("-|" . ?⊣)
-                ;; "\\bigcirc" no good rendering
-                ("|=" . ?⊨)
-                ;;"=|" no good rendering
-                ("\\sim" . ?∼)
-                ("->" . ?⭢)
-                ("<-" . ?⭠)
-                ("\\simeq" . ?≃)
-                ("\\cap" . ?∩)          ("\\intersect" . ?∩)
-                ("\\cup" . ?∪)          ("\\union" . ?∪)
-                ("\\asymp" . ?≍)
-                ("\\sqcap" . ?⊓)
-                ("\\sqcup" . ?⊔)
-                ("\\approx" . ?≈)
-                ("(+)" . ?⊕)            ("\\oplus" . ?⊕)
-                ("\\uplus" . ?⊎)
-                ("\\cong" . ?≅)
-                ("(-)" . ?⊖)            ("\\ominus" . ?⊖)
-                ("\\X" . ?×)            ("\\times" . ?×)
-                ("\doteq" . ?≐)
-                ("(.)" . ?⊙)
-                ;;
-                ;;
-                ;;
-                ("\\E" . ?∃)
-                ("\\A" . ?∀)
-                ;;("\\EE" . ?∃)
-                ;;("\\A" . ?∀)
-                ("LAMBDA" . ?λ)
-                ))
+	      '(
+		("/\\" . ?∧)          ("\\land" . ?∧)
+		("\\/" . ?∨)          ("\\lor" . ?∨)
+		("=>" . ?⇒)
+		("~" . ?¬)
+		("\\lnot" . ?¬)       ("\\neg" . ?¬)
+		("<=>" . ?≡)          ("\equiv" . ?≡)
+		("==" . ?≜)
+		("\\in" . ?∈)
+		("\\notin" . ?∉)
+		("#" . ?≠)            ("/=" . ?≠)
+		("<<" . ?⟨)
+		(">>" . ?⟩)
+		("[]" . ?□)
+		("<>" . ?◇)
+		("<=" . ?≤)           ("\\leq" . ?≤)
+		(">=" . ?≥)           ("\\geq" . ?≥)
+		("~>" . ?↝)
+		("\\ll" . ?\《)
+		("\\gg" . ?\》)
+		("-+->" . ?⇸)
+		("\\prec" . ?≺)
+		("\\succ" . ?≻)
+		("|->" . ?↦)
+		("\\preceq" . ?⋞)
+		("\\succeq" . ?≽)
+		("\\div" . ?÷)
+		("\\subseteq" . ?⊆)
+		("\\supseteq" . ?⊇)
+		("\\cdot" . ?⋅)
+		("\\subset" . ?⊂)
+		("\\supset" . ?⊃)
+		("\\o" . ?∘) ("\\circ" . ?∘)
+		("\\sqsubset" . ?⊏)
+		("\\sqsupset" . ?⊐)
+		("\\bullet" . ?•)
+		("\\sqsubseteq" . ?⊑)
+		("\\sqsupseteq" . ?⊒)
+		("\\star" . ?⋆)
+		("|-" . ?⊢)
+		("-|" . ?⊣)
+		;; "\\bigcirc" no good rendering
+		("|=" . ?⊨)
+		;;"=|" no good rendering
+		("\\sim" . ?∼)
+		("->" . ?⭢)
+		("<-" . ?⭠)
+		("\\simeq" . ?≃)
+		("\\cap" . ?∩)          ("\\intersect" . ?∩)
+		("\\cup" . ?∪)          ("\\union" . ?∪)
+		("\\asymp" . ?≍)
+		("\\sqcap" . ?⊓)
+		("\\sqcup" . ?⊔)
+		("\\approx" . ?≈)
+		("(+)" . ?⊕)            ("\\oplus" . ?⊕)
+		("\\uplus" . ?⊎)
+		("\\cong" . ?≅)
+		("(-)" . ?⊖)            ("\\ominus" . ?⊖)
+		("\\X" . ?×)            ("\\times" . ?×)
+		("\doteq" . ?≐)
+		("(.)" . ?⊙)
+		;;
+		;;
+		;;
+		("\\E" . ?∃)
+		("\\A" . ?∀)
+		;;("\\EE" . ?∃)
+		;;("\\A" . ?∀)
+		("LAMBDA" . ?λ)
+		))
 
 
-  (prettify-symbols-mode)  
+  (prettify-symbols-mode)
   )
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist
-             '("\\.tla\\'" . tla-mode))
+	     '("\\.tla\\'" . tla-mode))
 
 (provide 'tla-mode)
 
